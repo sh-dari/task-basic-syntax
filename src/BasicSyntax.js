03a44255c725e3013438d17330f616e17d2e725c
Дарья Шипилова <sh.dari@list.ru>
2022-10-16 01:08:07 +0300

    task-basic-syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -1,8 +1,15 @@
 export function romanToInteger(str) {
+    const roman = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
     let result = 0;
-    /*
-    Напишите код, преобразующий число из римской записи в арабскую.
-    На вход вы получите строку, ответом должно быть число.
-     */
+    str = str.toUpperCase().split('');
+    for (let i = 0; i < str.length; i++) {
+        const curr = roman[str[i]];
+        const next = roman[str[i + 1]];
+        if (curr < next) {
+            result -= curr;
+        } else {
+            result += curr;
+        }
+    }
     return result;
 }
